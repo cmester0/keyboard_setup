5fd932b0c17a7d8d5374dfa5dd1bcd8497e6507d
Lasse <lasse@letager.dk>
2022-04-12 00:38:18 +0200

    crkbd_minimal

---
+++ crkbd_minimalist_layout/keymap.c
@@ -0,0 +1,97 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _SYM 1 
+#define _NUM 2
+#define _NAV 3
+#define _COM 4
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  SYM,
+  NUM,
+  NAV,
+  COM,
+};
+
+/* // Shortcut to make keymap more readable */ 
+#define L_HOME0 LCTL_T(KC_A)
+#define L_HOME1 LT(_NAV,    KC_S)
+#define L_HOME2 LSFT_T(KC_D)
+#define L_HOME3 LT(_COM,    KC_F)
+#define L_HOME4 LT(_NUM,    KC_SPC)
+
+#define R_HOME0 RCTL_T(KC_MINUS)
+#define R_HOME1 LT(_NAV,    KC_L)
+#define R_HOME2 RSFT_T(KC_K)
+#define R_HOME3 LT(_COM,    KC_J)
+#define R_HOME4 LT(_SYM,    KC_BSPC)
+
+#include <stdio.h>
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,   XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, L_HOME0, L_HOME1, L_HOME2, L_HOME3,  KC_G,                         KC_H,  R_HOME3, R_HOME2, R_HOME1, R_HOME0, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_LALT, L_HOME4, XXXXXXX ,    XXXXXXX, R_HOME4, KC_TAB
+                                      //`--------------------------'  `--------------------------'
+
+  ),
+
+  [_SYM] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_ASTR, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_LALT, L_HOME4, XXXXXXX ,    XXXXXXX, R_HOME4, KC_TABa
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_NUM] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  KC_4,    KC_5,    KC_6,   XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  KC_1,    KC_2,    KC_3,   XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  KC_7,    KC_8,    KC_9,   XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_LALT, L_HOME4, XXXXXXX ,    XXXXXXX,  KC_0,   KC_TAB
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_NAV] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_UP  ,KC_RIGHT, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_DOWN, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_LALT, L_HOME4, XXXXXXX ,    XXXXXXX, R_HOME4, KC_TAB
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_COM] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , RESET  ,                      RESET  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+     KC_LCTRL, XXXXXXX, XXXXXXX, KC_ENT , XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_ENT , KC_VOLU, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_F11 , KC_F12 , 
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_LALT, L_HOME4, XXXXXXX ,    XXXXXXX, KC_DEL,  KC_TAB
+                                      //`--------------------------'  `--------------------------'
+  )
+};
---
+++ crkbd_minimalist_layout/rules.mk
@@ -0,0 +1,4 @@
+NKRO_ENABLE = yes
+# UNICODE_ENABLE = yes
+# UNICODEMAP_ENABLE = yes
+
