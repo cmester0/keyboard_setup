b814339700797c47f7819b38c6812f868dcf3210
Lasse Letager Hansen <letager@cs.au.dk>
2025-01-16 10:58:29 +0100

    Fixed keyboard setup for redox

--- minimalist_layout/config.h
+++ minimalist_layout/config.h
@@ -38,8 +38,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 160
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+/* // Prevent normal rollover on alphas from accidentally triggering mods. */
+/* #define IGNORE_MOD_TAP_INTERRUPT */
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
+
+#define BACKLIGHT_ON_STATE 0
